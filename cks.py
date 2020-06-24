611eba5d371fa2a328797cd6678814807dbed24a
Vivien Richter <vivien-richter@outlook.de>
2020-06-24 14:45:39 +0200

    added: extraction and filename

--- cks.py
+++ cks.py
@@ -1,4 +1,12 @@
+import sys
+from zipfile import ZipFile
 from xml.etree.ElementTree import ElementTree, Element, SubElement, Comment
+
+# Get filename
+filename = sys.argv[1].split(".")[0]
+
+# Extract Krita document
+ZipFile(filename + ".kra").extractall(filename)
 
 # Prepare
 svg = Element("svg")
@@ -7,5 +15,6 @@ svg.set("xmlns:xlink", "http://www.w3.org/1999/xlink")
 svg.set("xmlns:krita", "http://krita.org/namespaces/svg/krita")
 svg.set("xmlns:sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd")
 
+
 # Save
-ElementTree(svg).write("merged.svg")
+ElementTree(svg).write(filename + ".svg")
