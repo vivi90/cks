10c3275086b39d26c785978d6503241477c4db64
Vivien Richter <vivien-richter@outlook.de>
2020-06-24 17:44:48 +0200

    improved: layer detection

--- cks.py
+++ cks.py
@@ -2,11 +2,20 @@ from sys import argv as argument
 from zipfile import ZipFile
 from xml.etree.ElementTree import ElementTree, Element, SubElement, Comment
 from shutil import rmtree as delete
-import xml.etree.ElementTree as et
 
 class Application:
+    NAMESPACE_KRITA = "http://www.calligra.org/DTD/krita"
+
     def __init__(self, filename):
         self.filename = filename
+        self.prepare()
+
+    def prepare(self):
+        self.svg = Element("svg")
+        self.svg.set("xmlns", "http://www.w3.org/2000/svg")
+        self.svg.set("xmlns:xlink", "http://www.w3.org/1999/xlink")
+        self.svg.set("xmlns:krita", "http://krita.org/namespaces/svg/krita")
+        self.svg.set("xmlns:sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd")
 
     def extractKritaDocument(self):
         ZipFile(self.filename + ".kra").extractall(self.filename)
@@ -15,38 +24,33 @@ class Application:
         delete(self.filename)
 
     def save(self):
-        svg = Element("svg")
-        svg.set("xmlns", "http://www.w3.org/2000/svg")
-        svg.set("xmlns:xlink", "http://www.w3.org/1999/xlink")
-        svg.set("xmlns:krita", "http://krita.org/namespaces/svg/krita")
-        svg.set("xmlns:sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd")
-        ElementTree(svg).write(self.filename + ".svg")
+        ElementTree(self.svg).write(self.filename + ".svg")
 
-    def findLayers(self):
-        structure = et.parse(self.filename + "/maindoc.xml")
-        root = structure.getroot()
-        namespace = {"krita": "http://www.calligra.org/DTD/krita"}
+    def mergeLayers(self):
+        namespace = {"krita": self.NAMESPACE_KRITA}
+        root = ElementTree(ElementTree().parse(self.filename + "/maindoc.xml")).getroot()
+        imageName = root.find("./krita:IMAGE", namespace).get("name")
         layers = root.find("./krita:IMAGE/krita:layers", namespace)
-        def recur(layer):
+        def walkRecursive(layer):
             if layer.get("visible") == "1":
                 if layer.get("nodetype") == "grouplayer":
                     # Sublayers
                     for subLayer in layer.find("./krita:layers", namespace):
-                        recur(subLayer)
+                        walkRecursive(subLayer)
                 else:
                     if layer.get("nodetype") == "shapelayer":
-                        print(layer.get("name"))
+                        self.addLayer(self.filename + "/" + imageName + "/layers/" + layer.get("filename") + "." + layer.get("nodetype") + "/content.svg")
             return
-        recur(layers)
         for layer in layers:
             if layer.get("visible") == "1":
-                recur(layer)
-        #print([elem.tag for elem in root.iter()])
+                walkRecursive(layer)
 
+    def addLayer(self, path):
+        print(path)
 
 export = Application(argument[1].split(".")[0])
 export.extractKritaDocument()
-export.findLayers()
-#export.save()
+export.mergeLayers()
+export.save()
 #export.cleanup()
 del export
