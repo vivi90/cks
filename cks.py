32ed1fea1e47ec27cb959785b92cf60bc9f5f881
Vivien Richter <vivien-richter@outlook.de>
2020-06-24 16:52:20 +0200

    added: layer detection

--- cks.py
+++ cks.py
@@ -2,6 +2,7 @@ from sys import argv as argument
 from zipfile import ZipFile
 from xml.etree.ElementTree import ElementTree, Element, SubElement, Comment
 from shutil import rmtree as delete
+import xml.etree.ElementTree as et
 
 # Get filename
 filename = argument[1].split(".")[0]
@@ -20,4 +21,24 @@ svg.set("xmlns:sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd")
 ElementTree(svg).write(filename + ".svg")
 
 # Cleanup
-delete(filename)
+#delete(filename)
+
+structure = et.parse(filename + "/maindoc.xml")
+root = structure.getroot()
+namespace = {"krita": "http://www.calligra.org/DTD/krita"}
+layers = root.find("./krita:IMAGE/krita:layers", namespace)
+def recur(layer):
+    if layer.get("visible") == "1":
+        if layer.get("nodetype") == "grouplayer":
+            # Sublayers
+            for subLayer in layer.find("./krita:layers", namespace):
+                recur(subLayer)
+        else:
+            if layer.get("nodetype") == "shapelayer":
+                print(layer.get("name"))
+    return
+recur(layers)
+for layer in layers:
+    if layer.get("visible") == "1":
+        recur(layer)
+#print([elem.tag for elem in root.iter()])
